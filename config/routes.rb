95f0721706e5df02e4976fd4ef52cfdf84d62cac
Unknown <iwaita2ya@gmail.com>
2017-10-03 13:48:51 +0900

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :field_records
+  resources :field_record_details
+  match 'field_records/export/:id' => 'field_records#export_pgpi', via: [:get, :post], defaults: {format: 'xml'}
+
 end
